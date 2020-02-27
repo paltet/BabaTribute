9d785e3b752c5d0fba53d1f4c0e1a41f980dfb29
paltet <pau.altet.pujol@hotmail.com>
2020-02-27 20:45:25 +0100

    Commit 2

--- BabaTribute/Game.h
+++ BabaTribute/Game.h
@@ -2,7 +2,40 @@
 class Game
 {
 public:
-	Game();
-	~Game();
+	Game() {};
+
+	static Game &instance()
+	{
+		static Game G;
+		return G;
+	}
+
+	void init();
+	bool update(int deltaTime);
+	void render();
+
+	void keyPressed(int key);
+	void keyReleased(int key);
+	void specialKeyPressed(int key);
+	void specialKeyReleased(int key);
+	void mouseMove(int x, int y);
+	void mousePress(int button);
+	void mouseRelease(int button);
+
+	bool getKey(int key) const;
+	bool getSpecialKey(int key) const;
+
+private:
+	bool bPlay;
+	//enum { STATE_MENU, STATE_PLAYING, STATE_CREDITS };
+	//int state;
+
+	//Menu menu;
+	//Scene scene;
+	//Credits credits;
+
+	bool keys[256], specialKeys[256]; // Store key states so that 
+									  // we can have access at any time
+
 };
 

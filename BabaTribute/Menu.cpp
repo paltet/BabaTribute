bf8090c2ed32e4b5d592316de28cb1adbe3cdb9b
paltet <pau.altet.pujol@hotmail.com>
2020-03-02 13:03:31 +0100

    menu

--- BabaTribute/Menu.cpp
+++ BabaTribute/Menu.cpp
@@ -1,17 +1,24 @@
 #include "Menu.h"
+#include <iostream>
 
 Menu::Menu()
 {
-	currentTime = 0.f;
+	pointer = nullptr;
 }
-
 
 Menu::~Menu()
 {
+	if (pointer != nullptr) delete pointer;
 }
 
 void Menu::init() {
+	currentTime = 0.f;
+	state = PLAY;
 
+	if (!text.init("fonts/OpenSans-Regular.ttf"))
+		//if(!text.init("fonts/OpenSans-Bold.ttf"))
+		//if(!text.init("fonts/DroidSerif.ttf"))
+		cout << "Could not load font!!!" << endl;
 }
 
 void Menu::update(int deltaTime) {
@@ -19,5 +26,65 @@ void Menu::update(int deltaTime) {
 }
 
 void Menu::render() {
+	glm::mat4 modelview;
 
+	texProgram.use();
+
+	for (int i = 0; i < 4; i++) {
+		text.render(options[i], glm::vec2(CAMERA_WIDTH / 2 - 150, CAMERA_HEIGHT/4 + 50*i), TEXT_SIZE, glm::vec4(1.f, 1.f, 1.f, 1.f));
+	}
 }
+
+void Menu::initShaders()
+{
+	Shader vShader, fShader;
+
+	vShader.initFromFile(VERTEX_SHADER, "shaders/simple.vert");
+	if (!vShader.isCompiled())
+	{
+		cout << "Vertex Shader Error" << endl;
+		cout << "" << vShader.log() << endl << endl;
+	}
+	fShader.initFromFile(FRAGMENT_SHADER, "shaders/simple.frag");
+	if (!fShader.isCompiled())
+	{
+		cout << "Fragment Shader Error" << endl;
+		cout << "" << fShader.log() << endl << endl;
+	}
+	simpleProgram.init();
+	simpleProgram.addShader(vShader);
+	simpleProgram.addShader(fShader);
+	simpleProgram.link();
+	if (!simpleProgram.isLinked())
+	{
+		cout << "Shader Linking Error" << endl;
+		cout << "" << simpleProgram.log() << endl << endl;
+	}
+	simpleProgram.bindFragmentOutput("outColor");
+
+	vShader.free();
+	fShader.free();
+	vShader.initFromFile(VERTEX_SHADER, "shaders/texture.vert");
+	if (!vShader.isCompiled())
+	{
+		cout << "Vertex Shader Error" << endl;
+		cout << "" << vShader.log() << endl << endl;
+	}
+	fShader.initFromFile(FRAGMENT_SHADER, "shaders/texture.frag");
+	if (!fShader.isCompiled())
+	{
+		cout << "Fragment Shader Error" << endl;
+		cout << "" << fShader.log() << endl << endl;
+	}
+	texProgram.init();
+	texProgram.addShader(vShader);
+	texProgram.addShader(fShader);
+	texProgram.link();
+	if (!texProgram.isLinked())
+	{
+		cout << "Shader Linking Error" << endl;
+		cout << "" << texProgram.log() << endl << endl;
+	}
+	texProgram.bindFragmentOutput("outColor");
+}
+
--- BabaTribute/Menu.h
+++ BabaTribute/Menu.h
@@ -1,4 +1,13 @@
-#pragma once
+
+#include "ShaderProgram.h"
+#include "TexturedQuad.h"
+#include "Text.h"
+
+#define CAMERA_WIDTH 480
+#define CAMERA_HEIGHT 480
+
+#define TEXT_SIZE 32
+
 class Menu
 {
 public:
@@ -10,6 +19,18 @@ public:
 	void render();
 
 private:
+	void initShaders();
+
+private:
 	float currentTime;
+	Text text;
+	TexturedQuad *pointer;
+
+	enum {PLAY, HOWTO, ABOUT, EXIT};
+	int state;
+
+	ShaderProgram simpleProgram, texProgram;
+	glm::mat4 projection;
+	string options[4] = { "PLAY", "HOW TO PLAY", "CREDITS", "EXIT" };
 };
 
